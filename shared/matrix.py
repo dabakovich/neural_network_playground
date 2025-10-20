b06a81708adbed923a7d1aa4ca8c8ff190c6aa15
David Tabaka <dabakovich@gmail.com>
2025-10-20 16:49:36 +0300

    refactor: enhance Matrix class with shape method and improve multiplication logic for various types

--- shared/matrix.py
+++ shared/matrix.py
@@ -4,7 +4,7 @@ from .vector import Vector
 class Matrix:
     vectors: list[Vector]
 
-    def __init__(self, vectors: list[Vector] or list[list[int | float]] = None):
+    def __init__(self, vectors: list[Vector] or list[list[int or float]] = None):
         if vectors is None:
             vectors = []
 
@@ -24,16 +24,55 @@ class Matrix:
     def scalar_multiply(self, scalar: float) -> "Matrix":
         return Matrix([vector * scalar for vector in self.vectors])
 
-    def __mul__(self, vector: Vector or list[float]) -> Vector:
-        if isinstance(vector, list):
-            vector = Vector(vector)
+    def shape(self):
+        return (len(self.vectors), len(self.vectors[0]))
 
-        return Vector([self.vectors[i] * vector for i in range(len(self.vectors))])
+    def __mul__(self, value: "Matrix" or Vector or list[float] or int or float):
+        """
+        Multiply matrix to value.
 
-    def __sub__(self, other) -> "Matrix":
+        Could be another matrix, vector or scalar.
+        """
+        # Check if the value is another matrix
+        if isinstance(value, Matrix):
+            new_matrix_vectors: list[Vector] = []
+            transposed_matrix = value.transpose()
+
+            for row_index in range(len(self.vectors)):
+                new_row: list[int or float] = []
+
+                for column_index in range(len(value.vectors[0])):
+                    new_row.append(
+                        self.vectors[row_index]
+                        * transposed_matrix.vectors[column_index]
+                    )
+
+                new_matrix_vectors.append(Vector(new_row))
+
+            return Matrix(new_matrix_vectors)
+
+        # Otherwise we have a vector or array of numbers or scalar
+
+        # Check if it's an array of numbers, then transform them into Vector
+        if isinstance(value, list):
+            value = Vector(value)
+
+        # Check if the value is vector, then multiply by the vector
+        if isinstance(value, Vector):
+            return Vector([self.vectors[i] * value for i in range(len(self.vectors))])
+
+        # Otherwise we have scalar
+        return Matrix([vector * value for vector in self.vectors])
+
+    def __sub__(self, other: "Matrix") -> "Matrix":
         return Matrix(
             [self.vectors[i] - other.vectors[i] for i in range(len(self.vectors))]
         )
 
     def __str__(self):
-        return "\n".join([str(vector) for vector in self.vectors])
+        return (
+            "[\n" + ("\n".join(["  " + str(vector) for vector in self.vectors])) + "\n]"
+        )
+
+    def __repr__(self):
+        return self.__str__()
