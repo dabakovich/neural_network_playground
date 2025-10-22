679750a36dd51e7e77304f79d03d9a069ee88678
David Tabaka <dabakovich@gmail.com>
2025-10-22 13:56:50 +0300

    refactor: separate layer building logic into distinct functions and update return types for clarity

--- shoes_size_predictor/helpers.py
+++ shoes_size_predictor/helpers.py
@@ -2,33 +2,38 @@ from shared.helpers import get_random
 from shared.matrix import Matrix
 from shared.vector import Vector
 
-from .types import InputVector, Layer, LayerConfig
+from .types import InputVector, LayerConfig
 
 
-def build_layers(layer_configs: list[LayerConfig]) -> list[Layer]:
+def build_layers(layer_configs: list[LayerConfig]) -> list[Matrix]:
     """
     Builds neural network layers with biases
     """
-    layers: list[Layer] = []
+    weights_list: list[Matrix] = []
 
     # Iterate over all layers
-    for l_index in range(len(layer_configs)):
-        layer_config = layer_configs[l_index]
-        layer = Matrix()
+    for layer_config in layer_configs:
+        weights = build_layer(layer_config)
 
-        # Iterate over output neurons
-        for n_index in range(layer_config["output_size"]):
-            weights_and_bias = Vector(
-                [get_random(-1, 1) for _ in range(layer_config["input_size"])]
-            )
-            # Ad bias value
-            weights_and_bias.values.append(get_random(-1, 1))
+        weights_list.append(weights)
 
-            layer.vectors.append(weights_and_bias)
+    return weights_list
 
-        layers.append(layer)
 
-    return layers
+def build_layer(layer_config: LayerConfig) -> Matrix:
+    weights = Matrix()
+
+    # Iterate over output neurons
+    for n_index in range(layer_config["output_size"]):
+        weights_and_bias = Vector(
+            [get_random(-1, 1) for _ in range(layer_config["input_size"])]
+        )
+        # Ad bias value
+        weights_and_bias.values.append(get_random(-1, 1))
+
+        weights.vectors.append(weights_and_bias)
+
+    return weights
 
 
 def get_vector(input: InputVector) -> Vector:
@@ -42,8 +47,6 @@ def calculate_mse(output: InputVector, expected_output: InputVector) -> float:
     sum = 0
     output = get_vector(output)
     expected_output = get_vector(expected_output)
-
-    print(len(expected_output))
 
     subtraction = output - expected_output
 
